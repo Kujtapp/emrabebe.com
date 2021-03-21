2b81841d343d0f781a517e3525b16f5933e610c7
kushtra24 <kushtra24@gmail.com>
2021-03-21 10:19:13 +0100

    structural folder changes, nuxt auth, i18n, and html fixes in hompage

---
+++ components/FilterForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <form action="">
+        <!-- Checks -->
+        <div class="d-flex justify-content-center mt-3 mx-auto">
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+            <label class="form-check-label" for="inlineRadio1">Vajzë</label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+            <label class="form-check-label" for="inlineRadio2">Djalë</label>
+            </div>
+        </div>
+        <div class="mt-3">
+          <div class="form-group">
+            <select class="form-select form-control" aria-label="Default select example">
+                <option selected>Shkronja e parë</option>
+                <option value="1">A</option>
+                <option value="2">B</option>
+                <option value="3">C</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <select class="form-select form-control mt-4" aria-label="Default select example">
+                    <option selected>Origjina</option>
+                    <option value="1">Shqiperi</option>
+                    <option value="2">Kosovë</option>
+                    <option value="3">Maqeodni</option>
+            </select>
+          </div>
+
+            <div class="d-grid gap-2 mt-4">
+                <button type="submit" class="btn btn-brand">Kerko</button>
+            </div>
+          <nuxt-link to="/results" class="small text-muted mt-1">Shiko te gjitha pa filter</nuxt-link>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,67 @@
-<!--<i18n>-->
-<!--  {-->
-<!--    "en": {-->
-<!--    "welcome": "hello world!"-->
-<!--    },-->
-<!--    "fr": {-->
-<!--    "welcome": "something"-->
-<!--    }-->
-<!--  }-->
-<!--</i18n>-->
-
 <template>
-<div class="container">
   <div>
-    <div class="row">
-      <div class="col-md-4 search-baby-name">
-        <a class="navbar-brand col-md-4" href="#"><Logo /></a>
-        <h1>{{  $t('welcome') }}</h1>
-        <form @submit.prevent="nonVuexSubmit">
-          <div class="form-group">
-            <div class="form-group">
-              <select class="form-control" v-model="ngjyra">
-                <option disabled value="">zgjedh nje</option>
-                <option>e bardh</option>
-                <option>e zez</option>
-                <option>e kuqe</option>
-                <option>e kaltert</option>
-                <option>e omel</option>
-              </select>
-            </div>
-
-            <div class="here"> {{ ngjyra }}</div>
-
-            <div class="form-group">
-              <select class="form-control">
-                <option disabled value="">zgjedhe</option>
-                <option>10</option>
-                <option>20</option>
-                <option>Anglisht</option>
-                <option>Franceze</option>
-                <option>italiane</option>
-              </select>
-            </div>
-
-            <button type="submit" class="btn btn-primary mt-3">Submit</button>
-          </div>
-        </form>
+    <!-- mobile header     -->
+    <section id="header-mob">
+      <div class="container mt-3 mb-2 d-flex justify-content-between">
+        <i class="fas fa-search mt-1"></i>
+        <a href="index.html">
+          <logo />
+        </a>
+        <i class="fas fa-bars mt-1"></i>
       </div>
-      <div class="col-8">
-          <p v-for="hamam in jokes" :key="hamam.id">
-            {{ hamam.joke }}
-          </p>
+    </section>
+
+    <img src="~/assets/images/beba.jpg" alt="baby" class="baby" width="100%" height="100%">
+
+    <!-- mobile content -->
+    <section id="content-mob">
+      <div class="container">
+        <h1 class="text-center text-muted">Kërko emrin e</h1>
+        <h1 class="text-center text-muted">femijut te arshem..</h1>
+        <p class="small text-muted">femijuim.com është burimi më i madh në botë për
+          emrat e fëmijëve, që përmban një bazë të dhënash prej
+          70,000+ emra vajzash dhe emra djemsh (dhe emra asnjanës
+          gjinorë), mijëra lista gjithëpërfshirëse dhe krijuese
+          të emrave, forume miqësore, plus lajme dhe këshilla nga
+          ekspertët për të ndihmuar ju gjeni emrin perfekt për foshnjën tuaj.</p>
+      </div>
+
+    </section>
+
+    <!-- Desktop-Header -->
+    <div class="container mt-4">
+      <!-- nav bar -->
+      <nav class="navbar navbar-expand-lg navbar-light mb-5">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">Kërko Emër</a>
+            <a class="nav-link" href="#">Këshilla</a>
+            <a class="nav-link" href="#">Rreth nesh</a>
+            <a class="nav-link" href="#">Kyqu</a>
+            <div class="switcher-wrapper">
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- content -->
+      <div class="row">
+        <div class="col-lg-4 col-md-8 col-sm-10 filter-section">
+          <logo />
+          <h4 class="text-center mt-5">Si dëshironi të jet emri?</h4>
+          <hr class="divider mt-2 mb-5">
+          <filter-form />
+        </div>
       </div>
     </div>
-<!-- <p class="get-me-the-stuff" @click="getMeTheStuff">get me the stuff baby</p> -->
+    <p class="copyright small text-muted fixed-bottom"> © 2021 femijuim.com</p>
   </div>
-</div>
 </template>
 
 <script>
-
-export default {
-
-  data() {
-    return {
-      jokes: [],
-      ngjyra: ''
-    }
-  },
-
-  methods: {
-    async nonVuexSubmit() {
-
-      const config = {
-        headers: {
-          'Accept': 'application/json'
-        }
-      };
-
-        const url = `https://icanhazdadjoke.com/search`;
-
-        await this.$axios.get(url, config)
-        .then((response) => {
-          this.jokes = response.data.results;
-          console.log('jokes --> ', this.jokes);
-        })
-        .catch( (error) => {
-          console.log('nope', error);
-        });
-    },
-
+  export default {
   }
-
-}
-
 </script>
